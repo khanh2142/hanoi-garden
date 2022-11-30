fa521c043c4e5d11af44b9007b45d87fdbaada9e
khanh2142 <k2142a@gmail.com>
2022-11-30 19:48:35 +0700

    fix

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,3 +1,4 @@
+import { NextSeo } from "next-seo";
 import Advertise from "./Advertise";
 import Category from "./Category";
 import Comment from "./Comment";
@@ -7,9 +8,36 @@ import Header from "./Header";
 import ListProduct from "./ListProduct";
 import Recommend from "./Recommend";
 
+import wallpaper from "../public/images/wallpaper.png";
+
 const Home = () => {
   return (
     <>
+      <NextSeo
+        title="Ha Noi Garden"
+        description="Vườn hoa Hà Nội"
+        canonical="https://hanoi-garden.vercel.app"
+        openGraph={{
+          url: "https://hanoi-garden.vercel.app",
+          title: "Ha Noi Garden",
+          description: "Vườn hoa Hà Nội",
+          images: [
+            {
+              url: wallpaper.src,
+              width: 800,
+              height: 600,
+              alt: "Ha Noi Garden",
+              type: "image/png",
+            },
+          ],
+          siteName: "Ha Noi Garden",
+        }}
+        twitter={{
+          handle: "@hanoigarden",
+          site: "@hanoigarden",
+          cardType: "summary_large_image",
+        }}
+      ></NextSeo>
       <Header />
       <Advertise />
       <FeatureProduct />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,13 @@
-import Head from "next/head";
 import Home from "../components/Home";
 
 export default function App() {
   return (
     <div className="container mx-auto">
-      <Head>
+      {/* <Head>
         <title>Ha Noi Garden</title>
         <meta name="description" content="Ha Noi Garden" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head> */}
 
       <Home />
     </div>
