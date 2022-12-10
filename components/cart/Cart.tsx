8bf9e345b938dbc4e727fb6bf1578203760d839d
khanh2142 <k2142a@gmail.com>
2022-12-10 15:22:19 +0700

    add

--- components/cart/Cart.tsx
+++ components/cart/Cart.tsx
@@ -1,4 +1,8 @@
+import { Box, Input, InputLabel, Modal } from "@mui/material";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
 import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import api from "../../data/products.json";
 import { numberWithCommas } from "../../service/numberWithCommas";
 
@@ -6,6 +10,19 @@ const Cart = () => {
   const [data, setData] = useState([] as any);
   const [amount, setAmount] = useState(0);
   const [shipCost, setShipCost] = useState(30000);
+
+  const [open, setOpen] = useState(false);
+
+  const [formValue, setFormValue] = useState({
+    email: "",
+    phone: "",
+    username: "",
+    location: "",
+  });
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   useEffect(() => {
     reload();
@@ -87,8 +104,106 @@ const Cart = () => {
     localStorage.setItem("products", JSON.stringify(arr));
   };
 
+  const purchase = () => {
+    setOpen(true);
+  };
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid ",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const submit = () => {
+    toast.success("Cảm ơn bạn đã gửi thông tin. Chúng tôi sẽ liên hệ lại sau!");
+    handleClose();
+  };
+
+  useEffect(() => {
+    console.log(formValue);
+  }, [formValue]);
+
   return (
     <div className="mt-20">
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} component="form">
+          <FormControl className="w-full" required>
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input
+              id="email"
+              aria-describedby="my-helper-text"
+              name="email"
+              type="email"
+              onChange={(e: any) => {
+                setFormValue({
+                  ...formValue,
+                  email: e.target.value,
+                });
+              }}
+            />
+          </FormControl>
+          <FormControl className="w-full mt-5" required>
+            <InputLabel htmlFor="phone">Số điện thoại</InputLabel>
+            <Input
+              id="phone"
+              aria-describedby="my-helper-text"
+              name="phone"
+              onChange={(e: any) => {
+                setFormValue({
+                  ...formValue,
+                  phone: e.target.value,
+                });
+              }}
+            />
+          </FormControl>
+          <FormControl className="w-full mt-5" required>
+            <InputLabel htmlFor="username">Tên khách hàng</InputLabel>
+            <Input
+              id="username"
+              aria-describedby="my-helper-text"
+              name="username"
+              onChange={(e: any) => {
+                setFormValue({
+                  ...formValue,
+                  username: e.target.value,
+                });
+              }}
+            />
+          </FormControl>
+          <FormControl className="w-full mt-5" required>
+            <InputLabel htmlFor="location">Địa chỉ giao hàng</InputLabel>
+            <Input
+              id="location"
+              aria-describedby="my-helper-text"
+              name="location"
+              onChange={(e: any) => {
+                setFormValue({
+                  ...formValue,
+                  location: e.target.value,
+                });
+              }}
+              helperText="Vui lòng nhập địa chỉ!"
+            />
+          </FormControl>
+          <Box className="mt-5 flex justify-end">
+            <Button variant="contained" type="submit" onClick={submit}>
+              Gửi
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+
       <h3 className="uppercase text-2xl font-bold text-center text-sky-600">
         Giỏ hàng
       </h3>
@@ -149,7 +264,10 @@ const Cart = () => {
           >
             Tiếp tục mua sắm
           </a>
-          <button className="bg-amber-500 h-12 flex items-center justify-center px-5 text-white border-2 border-transparent hover:bg-transparent hover:text-amber-500 ease-linear duration-200 hover:border-amber-500 active:bg-green-400">
+          <button
+            className="bg-amber-500 h-12 flex items-center justify-center px-5 text-white border-2 border-transparent hover:bg-transparent hover:text-amber-500 ease-linear duration-200 hover:border-amber-500 active:bg-green-400"
+            onClick={purchase}
+          >
             Thanh toán
           </button>
         </div>
