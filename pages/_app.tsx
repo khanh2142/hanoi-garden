4760ecb7648268fa7dfed9add55536c445921d15
khanh2142 <k2142a@gmail.com>
2022-12-13 21:28:16 +0700

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,23 @@
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
-import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import ScrollTop from "../components/layout/ScrollTop";
-import store from "../store/store";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {}, []);
 
   return (
-    <Provider store={store}>
-      <div className="container mx-auto">
-        <Header />
-        <Component {...pageProps} />
-        <ScrollTop />
-        <Footer />
+    <div className="container mx-auto">
+      <Header />
+      <Component {...pageProps} />
+      <ScrollTop />
+      <Footer />
 
-        <ToastContainer autoClose={1000} />
-      </div>
-    </Provider>
+      <ToastContainer autoClose={1000} />
+    </div>
   );
 }
