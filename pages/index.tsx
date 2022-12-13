08f171e02457233230efc2492dfee3de32af1818
khanh2142 <k2142a@gmail.com>
2022-12-13 17:36:49 +0700

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,6 @@ import Home from "../components/home/Home";
 export default function App() {
   return (
     <div className="container mx-auto">
-      {/* <Head>
-        <title>Ha Noi Garden</title>
-        <meta name="description" content="Ha Noi Garden" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
-
       <Home />
     </div>
   );
