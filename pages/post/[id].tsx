11f2b0c831cc16eb0c031f5d832b977674d4c133
khanh2142 <k2142a@gmail.com>
2022-12-14 09:36:33 +0700

    add

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -1,4 +1,5 @@
 import parse from "html-react-parser";
+import { NextSeo } from "next-seo";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import api from "../../data/posts";
@@ -11,7 +12,7 @@ interface Post {
 }
 
 const DetailPost = () => {
-  const router :any = useRouter();
+  const router: any = useRouter();
 
   const { id } = router.query;
 
@@ -37,13 +38,50 @@ const DetailPost = () => {
   }, [id]);
 
   return (
-    <div className="mt-20 flex flex-col gap-4 items-center">
-      <h3 className="text-center text-2xl text-rose-500  font-bold">
-        {data.title}
-      </h3>
-      <img src={data.image} alt={data.title} className="w-2/3 h-100" />
-      <p className="leading-8">{parse(data.desc)}</p>
-    </div>
+    <>
+      <NextSeo
+        title={data.title}
+        description={data.title}
+        canonical={`https://hanoi-garden.vercel.app/post/${data.id}`}
+        openGraph={{
+          url: `https://hanoi-garden.vercel.app/post/${data.id}`,
+          title: data.title,
+          description: data.desc,
+          images: [
+            {
+              url: data.image,
+              width: 800,
+              height: 600,
+              alt: data.title,
+              type: "image/*",
+            },
+          ],
+          siteName: "Hà Nội Garden",
+        }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: "none",
+          maxVideoPreview: -1,
+        }}
+        additionalMetaTags={[
+          {
+            property: "dc:creator",
+            content: "Nguyễn Bá Khánh",
+          },
+        ]}
+      />
+      <div className="mt-20 flex flex-col gap-4 items-center">
+        <h3 className="text-center text-2xl text-rose-500  font-bold">
+          {data.title}
+        </h3>
+        <img src={data.image} alt={data.title} className="w-2/3 h-100" />
+        <p className="leading-8">{parse(data.desc)}</p>
+      </div>
+    </>
   );
 };
 
