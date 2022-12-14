2250d5b19f8112f9d0eb13320cf68992a9a986d6
khanh2142 <k2142a@gmail.com>
2022-12-14 09:32:02 +0700

    fix

--- pages/product.tsx
+++ pages/product.tsx
@@ -1,11 +1,18 @@
 import { NextSeo } from "next-seo";
 import ProductContainer from "../components/product/ProductContainer";
 
+import Head from "next/head";
 import productIcon from "public/images/wallpaper.png";
 
 const Product = () => {
   return (
     <div className="container mx-auto">
+      <Head>
+        <meta
+          name="keywords"
+          content="Hà Nội Garden, Hà Nội Garden Sản phẩm, Sản phẩm, Products , product"
+        />
+      </Head>
       <NextSeo
         title="Danh sách sản phẩm"
         description="Danh sách sản phẩm"
@@ -25,6 +32,21 @@ const Product = () => {
           ],
           siteName: "Hà Nội Garden",
         }}
+        robotsProps={{
+          nosnippet: true,
+          notranslate: true,
+          noimageindex: true,
+          noarchive: true,
+          maxSnippet: -1,
+          maxImagePreview: "none",
+          maxVideoPreview: -1,
+        }}
+        additionalMetaTags={[
+          {
+            property: "dc:creator",
+            content: "Nguyễn Bá Khánh",
+          },
+        ]}
       />
 
       <ProductContainer />
